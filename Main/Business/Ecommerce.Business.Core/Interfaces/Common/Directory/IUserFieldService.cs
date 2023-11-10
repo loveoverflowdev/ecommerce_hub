1c402ceafa19d08689f5a98bb0b646f6a617b4b2
Asuraman <manhpdbiz@gmail.com>
2023-11-10 13:53:58 +0700

    add interfaces.common for business core

---
+++ Main/Business/Ecommerce.Business.Core/Interfaces/Common/Directory/IUserFieldService.cs
@@ -0,0 +1,13 @@
+using Grand.Domain;
+
+namespace Grand.Business.Core.Interfaces.Common.Directory
+{
+    /// <summary>
+    /// User fields service interface
+    /// </summary>
+    public interface IUserFieldService
+    {
+        Task SaveField<TPropType>(BaseEntity entity, string key, TPropType value, string storeId = "");
+        Task<TPropType> GetFieldsForEntity<TPropType>(BaseEntity entity, string key, string storeId = "");
+    }
+}
